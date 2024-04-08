2f724ae55ea20103b2ef1b0ab925ed43e55ba57a
ImCrimP <pjmountin@gmail.com>
2024-04-07 19:41:28 -0500

    first commit

---
+++ frontend/src/components/LoadingIndicator.jsx
@@ -0,0 +1,11 @@
+import "../styles/LoadingIndicator.scss";
+
+const LoadingIndicator = () => {
+  return (
+    <div className="loader-container">
+      <div className="loader"></div>
+    </div>
+  );
+};
+
+export default LoadingIndicator;
